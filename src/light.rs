dc9f1371191b0d0420f50033a13e59b2fd6dad50
jjant <jantonielli@itba.edu.ar>
2021-11-13 23:48:44 +0000

    Chapter 6: Complete

---
+++ src/light.rs
@@ -0,0 +1,32 @@
+use crate::color::Color;
+use crate::tuple::Tuple;
+
+#[derive(Clone, Copy)]
+pub struct Light {
+    pub position: Tuple,
+    pub intensity: Color,
+}
+
+impl Light {
+    pub fn point_light(position: Tuple, intensity: Color) -> Self {
+        Self {
+            position,
+            intensity,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a_point_light_has_a_position_and_intensity() {
+        let intensity = Color::new(1., 1., 1.);
+        let position = Tuple::point(0., 0., 0.);
+        let light = Light::point_light(position, intensity);
+
+        assert_eq!(light.position, position);
+        assert_eq!(light.intensity, intensity);
+    }
+}
--- src/ray.rs
+++ src/ray.rs
@@ -1,41 +1,12 @@
 #![allow(dead_code)]
 use crate::matrix4::Matrix4;
+use crate::sphere::Sphere;
 use crate::{misc::approx_equal, tuple::Tuple};
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Sphere {
-    transform: Matrix4,
-}
-
-impl Sphere {
-    pub fn new() -> Self {
-        Self {
-            transform: Matrix4::identity(),
-        }
-    }
-
-    pub fn set_transform(&mut self, transform: Matrix4) {
-        self.transform = transform
-    }
-
-    pub fn normal_at(self, world_point: Tuple) -> Tuple {
-        let inverse_transform = self.transform.inverse().unwrap();
-        let object_point = inverse_transform * world_point;
-
-        let object_normal = object_point - Tuple::point(0., 0., 0.);
-
-        let mut world_normal = inverse_transform.transpose() * object_normal;
-        // TODO: Investigate what's up with setting the w = 0;
-        world_normal.w = 0.;
-
-        world_normal.normalize()
-    }
-}
 
 #[derive(Clone, Copy, Debug)]
 pub struct Intersection {
-    t: f64,
-    object: Sphere,
+    pub t: f64,
+    pub object: Sphere,
 }
 
 impl Intersection {
@@ -62,8 +33,8 @@ impl PartialEq for Intersection {
 
 #[derive(Clone, Copy, Debug)]
 pub struct Ray {
-    origin: Tuple,
-    direction: Tuple,
+    pub origin: Tuple,
+    pub direction: Tuple,
 }
 
 impl Ray {
@@ -71,7 +42,7 @@ impl Ray {
         Self { origin, direction }
     }
 
-    fn position(self, t: f64) -> Tuple {
+    pub fn position(self, t: f64) -> Tuple {
         self.origin + self.direction * t
     }
 
@@ -107,11 +78,8 @@ impl Ray {
 
 #[cfg(test)]
 mod tests {
-    use std::f64::consts::PI;
-
-    use crate::misc::approx_equal;
-
     use super::*;
+    use crate::misc::approx_equal;
 
     #[test]
     fn creating_and_querying_a_ray() {
@@ -291,23 +259,6 @@ mod tests {
     }
 
     #[test]
-    fn a_spheres_default_transformation() {
-        let s = Sphere::new();
-
-        assert_eq!(s.transform, Matrix4::identity());
-    }
-
-    #[test]
-    fn changing_a_spheres_transformation() {
-        let mut s = Sphere::new();
-        let t = Matrix4::translation(2., 3., 4.);
-
-        s.set_transform(t);
-
-        assert_eq!(s.transform, t);
-    }
-
-    #[test]
     fn intersecting_a_scaled_sphere_with_a_ray() {
         let r = Ray::new(Tuple::point(0., 0., -5.), Tuple::vector(0., 0., 1.));
         let mut s = Sphere::new();
@@ -331,61 +282,5 @@ mod tests {
         let xs = r.intersect(s);
 
         assert_eq!(xs.len(), 0);
-    }
-
-    #[test]
-    fn the_normal_on_a_sphere_at_a_point_on_the_x_axis() {
-        let s = Sphere::new();
-        let n = s.normal_at(Tuple::point(1., 0., 0.));
-        assert_eq!(n, Tuple::vector(1., 0., 0.));
-    }
-
-    #[test]
-    fn the_normal_on_a_sphere_at_a_point_on_the_y_axis() {
-        let s = Sphere::new();
-        let n = s.normal_at(Tuple::point(0., 1., 0.));
-        assert_eq!(n, Tuple::vector(0., 1., 0.));
-    }
-
-    #[test]
-    fn the_normal_on_a_sphere_at_a_point_on_the_z_axis() {
-        let s = Sphere::new();
-        let n = s.normal_at(Tuple::point(0., 0., 1.));
-        assert_eq!(n, Tuple::vector(0., 0., 1.));
-    }
-
-    #[test]
-    fn the_normal_on_a_sphere_at_a_nonaxial_point() {
-        let s = Sphere::new();
-        let n = s.normal_at(Tuple::point(
-            3_f64.sqrt() / 3.,
-            3_f64.sqrt() / 3.,
-            3_f64.sqrt() / 3.,
-        ));
-        assert_eq!(
-            n,
-            Tuple::vector(3_f64.sqrt() / 3., 3_f64.sqrt() / 3., 3_f64.sqrt() / 3.)
-        );
-    }
-
-    #[test]
-    fn computing_the_normal_on_a_translated_sphere() {
-        let mut s = Sphere::new();
-
-        s.set_transform(Matrix4::translation(0., 1., 0.));
-
-        let n = s.normal_at(Tuple::point(0., 1.70711, -0.70711));
-        assert_eq!(n, Tuple::vector(0., 0.70711, -0.70711));
-    }
-
-    #[test]
-    fn computing_the_normal_on_a_transformed_sphere() {
-        let mut s = Sphere::new();
-        let m = Matrix4::scaling(1., 0.5, 1.) * Matrix4::rotation_z(PI / 5.);
-
-        s.set_transform(m);
-
-        let n = s.normal_at(Tuple::point(0., 2_f64.sqrt() / 2., -2_f64.sqrt() / 2.));
-        assert_eq!(n, Tuple::vector(0., 0.97014, -0.24254));
     }
 }
